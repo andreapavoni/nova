2520f9618a241b0517a49264781bebd479216b1e
Andrea Pavoni <andrea.pavoni@gmail.com>
2015-09-05 23:40:48 +0200

    refactor .env-example.exs

--- .env-example.exs
+++ .env-example.exs
@@ -2,9 +2,10 @@
 # Copy this file to ".env.exs", it will be executed by "test" and "dev" configs.
 # It isn't the best solution, it just works.
 
-# Database credentials
-System.put_env("NOVA_DB_USER", "DB_USER")
-System.put_env("NOVA_DB_PASSWORD", "DB_PASSWORD")
-
-# Secret key. To generate a new one, run: `mix phoenix.gen.secret`
-System.put_env("NOVA_SECRET_KEY_BASE", "somethingsecretenough")
+for {key, value} <- %{
+  # Database credentials
+  "NOVA_DB_USER" => "DB_USER",
+  "NOVA_DB_PASSWORD" => "DB_PASSWORD",
+  # Secret key. To generate a new one, run: `mix phoenix.gen.secret`
+  "NOVA_SECRET_KEY_BASE" => "somethingsecretenough"
+}, do: System.put_env(key, value)
