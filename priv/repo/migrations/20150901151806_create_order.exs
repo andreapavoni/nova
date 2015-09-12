662b8e62c63131ff4eeb14a8b1e32201c6d8991b
Andrea Pavoni <andrea.pavoni@gmail.com>
2015-09-12 16:42:27 +0200

    fix create_order migration

--- priv/repo/migrations/20150901151806_create_order.exs
+++ priv/repo/migrations/20150901151806_create_order.exs
@@ -3,7 +3,7 @@ defmodule Nova.Repo.Migrations.CreateOrder do
 
   def change do
     create table(:nova_orders) do
-      add :total, :decimal
+      add :total, :decimal, precision: 10, scale: 2, default: 0.0
 
       timestamps
     end
