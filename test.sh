6165d05ce68bba432c8ac0b33f21fc5736c7d0e4
Andrea Pavoni <andrea.pavoni@gmail.com>
2015-09-16 16:13:40 +0200

    fix missing .env.exs for travis-ci

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+cat > .env.exs <<EOF
+for {key, value} <- %{
+  "NOVA_DB_USER" => "postgres",
+  "NOVA_DB_PASSWORD" => "",
+  "NOVA_SECRET_KEY_BASE" => "somethingsecretenough"
+}, do: System.put_env(key, value)
+EOF
+
 mix test || exit -1
