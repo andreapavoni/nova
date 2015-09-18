e24b9ffdfd410f7363783f98adcf43150d9ca4a4
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2015-09-18 13:34:33 -0500

    Fix build because missing env varible for travis

--- setup_travis_ci.sh
+++ setup_travis_ci.sh
@@ -4,6 +4,7 @@ cat > .env.exs <<EOF
 for {key, value} <- %{
   "NOVA_DB_USER" => "postgres",
   "NOVA_DB_PASSWORD" => "",
+  "NOVA_DB_PORT" => "5432",
   "NOVA_SECRET_KEY_BASE" => "somethingsecretenough"
 }, do: System.put_env(key, value)
 EOF
